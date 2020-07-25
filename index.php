b05cd8e4aefcd83ccfd24072916716a6e0df4ce4
Cedric Lima <limacedric@hotmail.fr>
2020-07-25 17:20:52 +0200

    Ajout Bootstrap

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mini-chat</title>
-    </head>
-    <style>
-    form
-    {
-        text-align:center;
-    }
-    </style>
-    <body>
-    
-    <form action="minichat.php" method="post">
-        <p>
-        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
-        <input type="submit" value="Envoyer" />
-	</p>
-    </form>
-
+<!doctype html>
+<html lang="fr">
+  <head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" crossorigin="anonymous">
+     <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+     <!-- Debut de page-->
+<div class="main">
+<div class="container">
+<form method="POST" action="minichat.php">
+  <div class="form-group">
+    <label for="pseudo">Pseudo : </label>
+    <input type="text" class="form-control" id="pseudo" name="pseudo" required>
+  </div>
+  <div class="form-group">
+    <label for="message">Message : </label>
+    <textarea class="form-control" id="message" name="message" required ></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Valider</button>
+</form>
+</div>
+<hr>
 <?php
 // Connexion à la base de données
 try
@@ -37,11 +42,26 @@ $reponse = $bdd->query('SELECT pseudo, message FROM minichat');
 // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
 while ($donnees = $reponse->fetch())
 {
-	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+?>
+
+<div class="container afficheMessage">
+   <ul class="list-group">
+      <li class="list-group-item"><?php echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>'; ?></li>
+   </ul>
+</div>
+<?php
 }
 
 $reponse->closeCursor();
 
 ?>
-    </body>
+</div>
+     <!-- Fin de page-->
+
+     <!-- Optional JavaScript -->
+     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+     <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+  </body>
 </html>
